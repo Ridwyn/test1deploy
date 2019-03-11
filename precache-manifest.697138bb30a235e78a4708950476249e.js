1f5ea8d99f3289cce88edcdc8ddbb262d84c46d6
Ridwyn <salihouridwyn@gmail.com>
2019-03-11 12:44:28 +0000

    init

---
+++ precache-manifest.697138bb30a235e78a4708950476249e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f418b0160774d0d5803789ed3e9b5c6",
+    "url": "/static/media/picture.2f418b01.png"
+  },
+  {
+    "revision": "3276eb8084fe5d1a9302da4b4666694c",
+    "url": "/static/media/banner2img.3276eb80.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b661b5b0f3248e8c557f",
+    "url": "/static/js/main.be645b58.chunk.js"
+  },
+  {
+    "revision": "17af3d555359a55f3197",
+    "url": "/static/js/2.1a5a7be0.chunk.js"
+  },
+  {
+    "revision": "b661b5b0f3248e8c557f",
+    "url": "/static/css/main.980637e2.chunk.css"
+  },
+  {
+    "revision": "a08eb715a1d0b6beb5672e62477631db",
+    "url": "/index.html"
+  }
+];
